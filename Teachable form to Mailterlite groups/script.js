66bafd8b64c5dfafbb25d745df96da979b6004d5
localhosf <97886809+localhosf@users.noreply.github.com>
2024-01-29 10:45:31 +0200

    Update script.js

--- Teachable form to Mailterlite groups/script.js
+++ Teachable form to Mailterlite groups/script.js
@@ -16,7 +16,7 @@ function createCourseMailingList(mailterliteCourseGroupID) {
         console.groupEnd('createCourseMailingList');
         return;
     }
-    if (!e.groupId || $.type(groupId) !== 'string' || !groupIdRegEx.test(groupId)) {
+    if (!groupId || $.type(groupId) !== 'string' || !groupIdRegEx.test(groupId)) {
         response.message.push('[groupId] is invalid or missing! expecting one or more numbers separated by comma!');
     }
 
