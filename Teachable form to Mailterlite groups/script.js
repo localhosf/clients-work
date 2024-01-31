949401120a503148d2e1aaeeefe21fbc6b56f109
localhosf <97886809+localhosf@users.noreply.github.com>
2024-01-31 02:38:00 +0200

    Update script.js

--- Teachable form to Mailterlite groups/script.js
+++ Teachable form to Mailterlite groups/script.js
@@ -1,6 +1,6 @@
 
 //TODO: update this to the URL of the Google Apps Script link;
-const API_endpoint = 'https://script.google.com/macros/s/AKfycbzJCmGnepH0vHZ_QKsIwMavX4Mxb1M6hUonPCCg15DkhFTAoOn9bOvBHxuSp_c_bd_yzg/exec';
+const API_endpoint = 'https://script.google.com/macros/s/AKfycbxrmvI1l2jPcn4TPEFOGTXcYHdlS5McpH2Ejmmi8xB7cn1fMzQ1N5vLqrA0ExbF7xpSjQ/exec';
 const sectionElementID = 'mailterlite-course-mailing-list';
 const emailRegEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 const groupIdRegEx = /^[0-9]+(?:,[0-9]+)*$/;
